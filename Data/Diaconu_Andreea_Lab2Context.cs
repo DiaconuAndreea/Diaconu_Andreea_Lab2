da39bac1c77c4edb8f72609a44cd71bc52ee6e28
andre <andre@LAPTOP-RO7TBIQ6>
2023-10-30 20:13:08 +0200

    Add project files.

---
+++ Data/Diaconu_Andreea_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Diaconu_Andreea_Lab2.Models;
+
+namespace Diaconu_Andreea_Lab2.Data
+{
+    public class Diaconu_Andreea_Lab2Context : DbContext
+    {
+        public Diaconu_Andreea_Lab2Context (DbContextOptions<Diaconu_Andreea_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Diaconu_Andreea_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Diaconu_Andreea_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Diaconu_Andreea_Lab2.Data;
+using Diaconu_Andreea_Lab2.Models;
+
+namespace Diaconu_Andreea_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Diaconu_Andreea_Lab2.Data.Diaconu_Andreea_Lab2Context _context;
+
+        public IndexModel(Diaconu_Andreea_Lab2.Data.Diaconu_Andreea_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
